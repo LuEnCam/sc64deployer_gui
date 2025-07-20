0fc75c9ac0a2d26ca5b269bd275186d5cde0a790
LuEnCam <67584779+LuEnCam@users.noreply.github.com>
2025-07-20 23:15:20 +0200

    start params implementation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import ( 
-    QApplication, QWidget, QGridLayout, QPushButton, QTextEdit, 
+    QApplication, QWidget, QGridLayout, QPushButton, QTextEdit, QLineEdit, QSpinBox,
     QHBoxLayout, QFileDialog, QLabel, QVBoxLayout, QMainWindow, 
     QGroupBox, QMenu, QWidgetAction
 )
@@ -8,7 +8,8 @@ from PyQt6.QtGui import QTextCursor
 from PyQt6.QtCore import QThread, QSettings, QSize
 from exec_runner import ExecWorker
 
-from sc64_lists_command_options import list_of_commands, list_of_options
+from sc64_lists_command_options import list_of_commands, list_of_options, list_of_parameters
+from sc64_lists_command_options import Types
 
 class SC64MainWidget(QWidget):
     def __init__(self):
@@ -33,15 +34,20 @@ class SC64MainWidget(QWidget):
         self.notify = True
 
         self.options_of_command = [] # test
+        self.parameters_of_options = [] # test
 
         self.button_group_commands = QGroupBox("Commands")
         self.group_options = QGroupBox("Options")
+        self.group_parameters = QGroupBox("Parameters")
         self.button_layout = QGridLayout()
         self.options_layout = QGridLayout()
+        self.parameter_layout = QGridLayout()
         self.button_group_commands.setLayout(self.button_layout)
         self.group_options.setLayout(self.options_layout)
+        self.group_parameters.setLayout(self.parameter_layout)
         self.glayout.addWidget(self.button_group_commands, 0, 0, 1, 1)
         self.glayout.addWidget(self.group_options, 1, 0, 1, 1)
+        self.glayout.addWidget(self.group_parameters, 2, 0, 1, 1)
         self.buttons = []
         i = 0
         j = 0
@@ -87,9 +93,10 @@ class SC64MainWidget(QWidget):
 
 
     def list_options(self, command):
-        for i in reversed(range(self.options_layout.count())): 
-            self.options_layout.itemAt(i).widget().setParent(None)
+        self.clear_options_layout()
+        self.clear_parameter_layout()
         options = [opt for opt in list_of_options if opt[0] == command and opt[1] != "---"]
+        print(f"Size of options: {len(options)}")
         if options:
             if self.options_of_command:
                 del self.options_of_command
@@ -97,11 +104,11 @@ class SC64MainWidget(QWidget):
             i = 0
             j = 0
             for opt in options:
-                str_of_opt_with_no_args = f"{opt[1]} {opt[2]}"
+                str_of_opt_with_no_args = f"{opt[1]}"
                 btn = QPushButton(str_of_opt_with_no_args)
-                btn.setToolTip(opt[3])
+                btn.setToolTip(opt[4])
                 #btn.clicked.connect(self.run_command)
-                btn.clicked.connect(lambda _, c=str_of_opt_with_no_args: print(f"Option '{c}' selected for command '{command}'"))
+                btn.clicked.connect(lambda _, option=opt: self.list_parameters(command, option))
                 btn.setEnabled(True)
                 self.options_layout.addWidget(btn, i, j)
                 j += 1
@@ -109,6 +116,68 @@ class SC64MainWidget(QWidget):
                     j = 0
                     i += 1
                 self.options_of_command.append(btn)
+        else: # If there are no options, run the command directly
+            self.run_command(command)
+
+
+    def list_parameters(self, command, option):
+        self.clear_parameter_layout()
+        print(f"Size of params: {option[2]}")
+        if option[2] > 0 or command == "upload" or command == "download" or command == "64dd":
+            if self.parameters_of_options:
+                del self.parameters_of_options
+            self.parameters_of_options = []
+
+            ## section for "upload" command (needs by default a path to ROM file)
+            i = 0
+            line_edit_of_rom = None
+            if command == "upload" or command == "download" or command == "64dd":
+                line_edit_of_rom = QLineEdit()
+                line_edit_of_rom.setPlaceholderText("Path to ROM file")
+                line_edit_of_rom.setDisabled(True)
+                btn = QPushButton("Select ROM Path")
+                btn.clicked.connect(lambda _: line_edit_of_rom.setText(QFileDialog.getOpenFileName(self, 'Select ROM File', '', 'ROM Files(*.z64 *.n64 *.v64 *.bin)')[0]))
+                self.parameter_layout.addWidget(line_edit_of_rom, i, 0, 1, 3)
+                self.parameter_layout.addWidget(btn, i, 3, 1, 1)
+                i += 1
+            j = 0
+            params = [opt for opt in list_of_parameters if opt[0] == command and opt[1] == option[1]]
+            if params: # Mostly for RADIO options
+                for param in params[0][2]:
+                    radio_btn = QPushButton(param)
+                    radio_btn.clicked.connect(lambda _, c=param: self.run_command(command, f"{option[1]} {c}"))
+                    self.parameter_layout.addWidget(radio_btn, i, j)
+                    j += 1
+                    if j >= 4:
+                        j = 0
+                        i += 1
+            else: # Mostly for non multiple choice options
+                if option[2] > 0:
+                    type_of_param: Types = option[3]
+                    btn = None
+                    if type_of_param == Types.TEXT:
+                        btn = QLineEdit()
+                        btn.setPlaceholderText(option[4])
+                    elif type_of_param == Types.PATH:
+                        btn = QPushButton("Select Path")
+                        btn.setToolTip(option[4])
+                        fdOptions = QFileDialog.Option.ShowDirsOnly
+                        btn.clicked.connect(lambda _,: 
+                                            self.run_command(command, f"{option[1]} {QFileDialog.getSaveFileName(self, "Save File", "", "All Files(*);;Text Files(*.txt)", options = fdOptions)[0]}") if command else "")                
+                    elif type_of_param == Types.SPINNER:
+                        btn = QSpinBox()
+                        btn.setRange(0, 65535)
+                    elif type_of_param == Types.ADDRESS:
+                        btn = QLineEdit()
+                        btn.setPlaceholderText("Enter address")
+                    self.parameter_layout.addWidget(btn, i, j)
+                    j += 1
+                    if j >= 4:
+                        j = 0
+                        i += 1
+                    self.parameters_of_options.append(btn)
+        else: # If there are no parameters, run the command directly
+            self.run_command(command, option[1])
 
 
     def run_command(self):
@@ -123,14 +192,24 @@ class SC64MainWidget(QWidget):
             btn.setEnabled(False)
         self.start_worker(cmd)
 
-    def start_worker(self, command, notify=True):
+    def run_command(self, command, options=""):
+        if not self.exe_path:
+            self.append_error("Please select sc64deployer.exe first.")
+            return
+        self.text_output.append(f"\n[CMD] {command} {options}")
+        self.text_output.moveCursor(QTextCursor.MoveOperation.End)
+        for btn in self.buttons:
+            btn.setEnabled(False)
+        self.start_worker(command, options=options)
+
+    def start_worker(self, command, options = "", notify=True):
         self.notify = notify
         # Clean up previous thread if any
         if self.worker_thread:
             self.worker_thread.quit()
             self.worker_thread.wait()
         # Set up new worker and thread
-        self.worker = ExecWorker(self.exe_path, command)
+        self.worker = ExecWorker(self.exe_path, command, list(options.split()) if options else [])
         self.worker_thread = QThread()
         self.worker.moveToThread(self.worker_thread)
         self.worker.output.connect(self.append_output)
@@ -153,6 +232,7 @@ class SC64MainWidget(QWidget):
         self.text_output.append(f"{text.strip()}")
         self.text_output.moveCursor(QTextCursor.MoveOperation.End)
 
+
     def process_finished(self):
         for btn in self.buttons:
             btn.setEnabled(True)
@@ -161,6 +241,14 @@ class SC64MainWidget(QWidget):
             self.worker_thread.wait()
             self.worker_thread = None
             self.worker = None
+
+    def clear_options_layout(self):
+        for i in reversed(range(self.options_layout.count())): 
+            self.options_layout.itemAt(i).widget().setParent(None)
+    
+    def clear_parameter_layout(self):
+        for i in reversed(range(self.parameter_layout.count())): 
+            self.parameter_layout.itemAt(i).widget().setParent(None)
 
 
 class SC64MainWindow():
--- sc64_lists_command_options.py
+++ sc64_lists_command_options.py
@@ -1,3 +1,16 @@
+from enum import Enum
+
+class Types(Enum):
+    TEXT = 1
+    PATH = 2
+    RADIO = 3
+    SPINNER = 4
+    ADDRESS = 5
+    HEX_4 = 6
+    ADDRESS_LENGTH_PATH = 7
+    PATH_RADIO = 8
+    PATH_HEX_4 = 9
+
 list_of_commands = [
     ["list", "List connected SC64 devices"],
     ["upload", "Upload ROM (and save) to the SC64"], 
@@ -23,66 +36,77 @@ list_of_commands = [
 # command: the command this option belongs to (or "---" for global options)
 # option: the option name (e.g., "--port")
 # argument_count: number of arguments this option takes (0 for no arguments)
+# type: the type of arguments (1 for text input, 2 for path file, 3 for radio buttons, 4 for spinner)
 # description: a brief description of the option
 list_of_options = [
-    ["---", "--port", 1, "Connect to SC64 device on provided local port"],
-    ["---", "--remote", 1, "Connect to SC64 device on provided remote address"],   
-    ["---", "--help", 0, "Print help"],
+    ["---", "--port", 1, Types.SPINNER ,"Connect to SC64 device on provided local port"],
+    ["---", "--remote", 1, Types.ADDRESS, "Connect to SC64 device on provided remote address"],   
+    ["---", "--help", 0, None, "Print help"],
 
     # list don't have options
 
-    ["upload", "--reboot", 0, "Attempt to reboot the console (requires specific support in the running game)"],
-    ["upload", "--save", 1, "Path to the save file <SAVE>"],
-    ["upload", "--save-type", 1, "Override autodetected save type <SAVE_TYPE> [possible values: none, eeprom4k, eeprom16k, sram, sram-banked, sram1m, flashram]"],
-    ["upload", "--direct", 0, "Use direct boot mode (skip bootloader)"],
-    ["upload", "--no-shadow", 0, "Do not put last 128 kiB of ROM inside flash memory (can corrupt non EEPROM saves)"],
-    ["upload", "--tv", 1, "Force TV type [possible values: pal, ntsc, mpal]"],
-    ["upload", "--cic-seed", 1, "Force CIC seed <CIC_SEED>"],
+    ["upload", "--reboot", 0, Types.PATH, "Attempt to reboot the console (requires specific support in the running game)"],
+    ["upload", "--save", 1, Types.PATH, "Path to the save file <SAVE>"],
+    ["upload", "--save-type", 1, Types.PATH_RADIO, "Override autodetected save type <SAVE_TYPE> [possible values: none, eeprom4k, eeprom16k, sram, sram-banked, sram1m, flashram]"],
+    ["upload", "--direct", 0, Types.PATH, "Use direct boot mode (skip bootloader)"],
+    ["upload", "--no-shadow", 0, Types.PATH, "Do not put last 128 kiB of ROM inside flash memory (can corrupt non EEPROM saves)"],
+    ["upload", "--tv", 1, Types.PATH_RADIO, "Force TV type [possible values: pal, ntsc, mpal]"],
+    ["upload", "--cic-seed", 1, Types.PATH_HEX_4, "Force CIC seed <CIC_SEED>"],
+    ["upload", "", 0, Types.PATH, "Just upload ROM without any options"],
 
-    ["download", "save", 1, "Download save and write it to file <PATH>"],
+    ["download", "save", 1, Types.PATH, "Download save and write it to file <PATH>"],
 
     # special case for 64DD that could have 2 or more inputs other than the options of the command
     # Usage: sc64deployer.exe 64dd [OPTIONS] <DDIPL> [DISK]...
-    ["64dd", "---", 0, "Upload 64DD IPL and DISK ROM.\nArguments:\n<DDIPL> Path to the 64DD IPL file.\n[DISK]...  Path to the 64DD disk file (.ndd format, can be specified multiple times)"],
-    ["64dd", "--rom", 1, "Path to the ROM cartridge file <ROM>"],
-    ["64dd", "--save", 1, "Path to the save file <SAVE> (also used by save writeback mechanism)"],
-    ["64dd", "--save-type", 1, "Override autodetected save type <SAVE_TYPE> [possible values: none, eeprom4k, eeprom16k, sram, sram-banked, sram1m, flashram]"],
-    ["64dd", "--direct", 0, "Use direct boot mode (skip bootloader)"],
-    ["64dd", "--tv", 1, "Force TV type [possible values: pal, ntsc, mpal]"],
-    ["64dd", "--cic-seed", 1, "Force CIC seed <CIC_SEED>"],
+    ["64dd", "---", 0, None, "Upload 64DD IPL and DISK ROM.\nArguments:\n<DDIPL> Path to the 64DD IPL file.\n[DISK]...  Path to the 64DD disk file (.ndd format, can be specified multiple times)"],
+    ["64dd", "--rom", 1, Types.PATH, "Path to the ROM cartridge file <ROM>"],
+    ["64dd", "--save", 1, Types.PATH, "Path to the save file <SAVE> (also used by save writeback mechanism)"],
+    ["64dd", "--save-type", 1, Types.RADIO, "Override autodetected save type <SAVE_TYPE> [possible values: none, eeprom4k, eeprom16k, sram, sram-banked, sram1m, flashram]"],
+    ["64dd", "--direct", 0, None, "Use direct boot mode (skip bootloader)"],
+    ["64dd", "--tv", 1, Types.RADIO, "Force TV type [possible values: pal, ntsc, mpal]"],
+    ["64dd", "--cic-seed", 1, Types.HEX_4, "Force CIC seed <CIC_SEED>"],
 
-    ["debug", "--save", 1, "Path to the save file <SAVE> (also used by save writeback mechanism)"],
-    ["debug", "--isv", 1, "Enable IS-Viewer64 and set listening address at ROM offset <offset> (in most cases it's fixed at 0x03FF0000)"],
-    ["debug", "--euc-jp", 0, "Use EUC-JP encoding for text printing"],
-    ["debug", "--no-writeback", 0, "Do not enable save writeback via USB"],
-    ["debug", "--init", 1, "List of commands to send after connecting to the SC64, semicolon separated (;)"],
+    ["debug", "--save", 1, Types.PATH, "Path to the save file <SAVE> (also used by save writeback mechanism)"],
+    ["debug", "--isv", 1, Types.ADDRESS, "Enable IS-Viewer64 and set listening address at ROM offset <offset> (in most cases it's fixed at 0x03FF0000)"],
+    ["debug", "--euc-jp", 0, None, "Use EUC-JP encoding for text printing"],
+    ["debug", "--no-writeback", 0, None, "Do not enable save writeback via USB"],
+    ["debug", "--init", 1, Types.TEXT, "List of commands to send after connecting to the SC64, semicolon separated (;)"],
 
-    ["dump", "---", 3, "Dump data from arbitrary location in SC64 memory space.\nArguments:\n<ADDRESS>  Starting memory address\n<LENGTH>   Dump length\n<PATH>     Path to the dump file"],
+    ["dump", "---", 3, Types.ADDRESS_LENGTH_PATH, "Dump data from arbitrary location in SC64 memory space.\nArguments:\n<ADDRESS>  Starting memory address\n<LENGTH>   Dump length\n<PATH>     Path to the dump file"],
 
-    ["sd", "ls", 1, "List files of a directory <PATH> on the SD card"],
-    ["sd", "stat", 1, "Display a file or directory status <PATH>"],
-    ["sd", "mv", 2, "Move or rename a file or directory <SOURCE> to <DESTINATION>"],
-    ["sd", "rm", 1, "Remove a file or empty directory <PATH>"],
-    ["sd", "download", 2, "Download a file <SOURCE> from the SD card to the PC <DESTINATION>"],
-    ["sd", "upload", 2, "Upload a file <SOURCE> from the PC to the SD card <DESTINATION>"],
-    ["sd", "mkfs", 0, "Format the SD card"],
+    ["sd", "ls", 1, Types.PATH, "List files of a directory <PATH> on the SD card"],
+    ["sd", "stat", 1, Types.PATH, "Display a file or directory status <PATH>"],
+    ["sd", "mv", 2, Types.PATH, "Move or rename a file or directory <SOURCE> to <DESTINATION>"],
+    ["sd", "rm", 1, Types.PATH, "Remove a file or empty directory <PATH>"],
+    ["sd", "download", 2, Types.PATH, "Download a file <SOURCE> from the SD card to the PC <DESTINATION>"],
+    ["sd", "upload", 2, Types.PATH, "Upload a file <SOURCE> from the PC to the SD card <DESTINATION>"],
+    ["sd", "mkfs", 0, None, "Format the SD card"],
 
     # info don't have options
 
     # reset don't have options
 
-    ["set", "rtc", 0, "Synchronize real time clock (RTC) on the SC64 with local system time"],    
-    ["set", "blink-on", 0, "Enable LED I/O activity blinking"], 
-    ["set", "blink-off", 0, "Disable LED I/O activity blinking"], 
+    ["set", "rtc", 0, None, "Synchronize real time clock (RTC) on the SC64 with local system time"],    
+    ["set", "blink-on", 0, None, "Enable LED I/O activity blinking"], 
+    ["set", "blink-off", 0, None, "Disable LED I/O activity blinking"], 
 
-    ["firmware", "info", 1, "Print metadata included inside SC64 firmware file <FIRMWARE>"],  
-    ["firmware", "backup", 1, "Download current SC64 firmware and save it to provided file <FIRMWARE>"],  
+    ["firmware", "info", 1, Types.PATH, "Print metadata included inside SC64 firmware file <FIRMWARE>"],  
+    ["firmware", "backup", 1, Types.PATH, "Download current SC64 firmware and save it to provided file <FIRMWARE>"],  
     # second argument is optional
-    ["firmware", "update", 2, "Update SC64 firmware from provided file <FIRMWARE>\n --use-flash-memory  Put firmware update in the Flash memory instead of SDRAM"],  
+    ["firmware", "update", 2, Types.PATH_RADIO, "Update SC64 firmware from provided file <FIRMWARE>\n --use-flash-memory  Put firmware update in the Flash memory instead of SDRAM"],  
 
     # test don't have options
 
     # fifo-read don't have options
 
-    ["server", "---", 1, "Expose SC64 device over network\nArguments:\n[ADDRESS]  Listen on provided address:port [default: 127.0.0.1:9064]"]
+    ["server", "---", 1, Types.ADDRESS, "Expose SC64 device over network\nArguments:\n[ADDRESS]  Listen on provided address:port [default: 127.0.0.1:9064]"]
+]
+
+# Parameters for commands (only useful for Types.RADIO or Types.PATH_RADIO)
+list_of_parameters = [
+    ["upload", "--save-type", ["none", "eeprom4k", "eeprom16k", "sram", "sram-banked", "sram1m", "flashram"]],
+    ["64dd", "--save-type", ["none", "eeprom4k", "eeprom16k", "sram", "sram-banked", "sram1m", "flashram"]],
+    ["upload", "--tv", ["pal", "ntsc", "mpal"]],
+    ["64dd", "--tv", ["pal", "ntsc", "mpal"]],
+    ["firmware", "--update", ["use-flash-memory"]],
 ]
