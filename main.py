e68910f250bea8555b988673187193b5ac6837e3
LuEnCam <67584779+LuEnCam@users.noreply.github.com>
2025-11-25 00:04:02 +0100

    Refactor command option handling and add run button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import shlex
 from PyQt6.QtWidgets import ( 
     QApplication, QWidget, QGridLayout, QPushButton, QTextEdit, QLineEdit, QSpinBox,
     QHBoxLayout, QFileDialog, QLabel, QVBoxLayout, QMainWindow, 
@@ -27,6 +28,8 @@ class SC64MainWidget(QWidget):
         self.clear_output_button = QPushButton("Clear Output")
         self.clear_output_button.clicked.connect(self.text_output.clear)
         self.group_output_layout.addWidget(self.clear_output_button, 0, 1, 1, 1)
+        self.run_command_button = QPushButton("Run Command")
+        self.run_command_button.setEnabled(False)
 
         
         self.exe_label = QLabel("No .exe selected.")
@@ -48,6 +51,7 @@ class SC64MainWidget(QWidget):
         self.glayout.addWidget(self.button_group_commands, 0, 0, 1, 1)
         self.glayout.addWidget(self.group_options, 1, 0, 1, 1)
         self.glayout.addWidget(self.group_parameters, 2, 0, 1, 1)
+        self.glayout.addWidget(self.run_command_button, 3, 0, 1, 1)
         self.buttons = []
         i = 0
         j = 0
@@ -56,8 +60,9 @@ class SC64MainWidget(QWidget):
             btn.setToolTip(cmd[1])
             #btn.clicked.connect(self.run_command)
             btn.clicked.connect(lambda _, c=cmd[0], b=btn: 
-                                (self.list_options(c), 
-                                 self.update_button_color(b)))
+                                (self.disable_run_button(),
+                                 self.list_options(c), 
+                                 self.update_button_color(b, self.buttons)))
             btn.setEnabled(False)
             self.button_layout.addWidget(btn, i, j)
             j += 1
@@ -72,8 +77,8 @@ class SC64MainWidget(QWidget):
 
         self.setLayout(self.glayout)
 
-    def update_button_color(self, btn: QPushButton):
-        for b in self.buttons:
+    def update_button_color(self, btn: QPushButton, button_list):
+        for b in button_list:
             b.setStyleSheet("")
         
         btn.setStyleSheet("background-color: green;")
@@ -108,7 +113,7 @@ class SC64MainWidget(QWidget):
                 btn = QPushButton(str_of_opt_with_no_args)
                 btn.setToolTip(opt[4])
                 #btn.clicked.connect(self.run_command)
-                btn.clicked.connect(lambda _, option=opt: self.list_parameters(command, option))
+                btn.clicked.connect(lambda _, option=opt, b=btn:( self.list_parameters(command, option), self.update_button_color(b, self.options_of_command)))
                 btn.setEnabled(True)
                 self.options_layout.addWidget(btn, i, j)
                 j += 1
@@ -117,10 +122,12 @@ class SC64MainWidget(QWidget):
                     i += 1
                 self.options_of_command.append(btn)
         else: # If there are no options, run the command directly
-            self.run_command(command)
+            #self.run_command(command)
+            self.enable_run_button(command)
 
 
     def list_parameters(self, command, option):
+        self.disable_run_button()
         self.clear_parameter_layout()
         print(f"Size of params: {option[2]}")
         if option[2] > 0 or command == "upload" or command == "download" or command == "64dd":
@@ -136,16 +143,31 @@ class SC64MainWidget(QWidget):
                 line_edit_of_rom.setPlaceholderText("Path to ROM file")
                 line_edit_of_rom.setDisabled(True)
                 btn = QPushButton("Select ROM Path")
-                btn.clicked.connect(lambda _: line_edit_of_rom.setText(QFileDialog.getOpenFileName(self, 'Select ROM File', '', 'ROM Files(*.z64 *.n64 *.v64 *.bin)')[0]))
+                params = [opt for opt in list_of_parameters if opt[0] == command and opt[1] == option[1]]
+                if params or option[2] != 0:
+                    btn.clicked.connect(lambda _: (
+                        line_edit_of_rom.setText(QFileDialog.getOpenFileName(self, 'Select ROM File', '', 'ROM Files(*.z64 *.n64 *.v64 *.bin)')[0]),
+                        self.disable_run_button()
+                    ))
+                else:
+                    btn.clicked.connect(lambda _: (
+                        line_edit_of_rom.setText(QFileDialog.getOpenFileName(self, 'Select ROM File', '', 'ROM Files(*.z64 *.n64 *.v64 *.bin)')[0]),
+                        self.enable_run_button(command, f"{shlex.quote(line_edit_of_rom.text())}"))
+                    )
                 self.parameter_layout.addWidget(line_edit_of_rom, i, 0, 1, 3)
                 self.parameter_layout.addWidget(btn, i, 3, 1, 1)
                 i += 1
             j = 0
-            params = [opt for opt in list_of_parameters if opt[0] == command and opt[1] == option[1]]
             if params: # Mostly for RADIO options
                 for param in params[0][2]:
                     radio_btn = QPushButton(param)
-                    radio_btn.clicked.connect(lambda _, c=param: self.run_command(command, f"{option[1]} {c}"))
+                    #radio_btn.clicked.connect(lambda _, c=param: self.run_command(command, f"{option[1]} {c}"))
+                    if line_edit_of_rom:
+                        radio_btn.clicked.connect(lambda _, c=param: 
+                                                  self.enable_run_button(command, f"{option[1]} {c} {shlex.quote(line_edit_of_rom.text())}"))
+                    else:
+                        radio_btn.clicked.connect(lambda _, c=param: 
+                                                  self.enable_run_button(command, f"{option[1]} {c}"))
                     self.parameter_layout.addWidget(radio_btn, i, j)
                     j += 1
                     if j >= 4:
@@ -158,12 +180,20 @@ class SC64MainWidget(QWidget):
                     if type_of_param == Types.TEXT:
                         btn = QLineEdit()
                         btn.setPlaceholderText(option[4])
-                    elif type_of_param == Types.PATH:
-                        btn = QPushButton("Select Path")
+                    elif type_of_param == Types.PATH_GET_LOCATION:
+                        btn = QPushButton("Select File")
                         btn.setToolTip(option[4])
                         fdOptions = QFileDialog.Option.ShowDirsOnly
-                        btn.clicked.connect(lambda _,: 
-                                            self.run_command(command, f"{option[1]} {QFileDialog.getSaveFileName(self, "Save File", "", "All Files(*);;Text Files(*.txt)", options = fdOptions)[0]}") if command else "")                
+                        #btn.clicked.connect(lambda _,: self.run_command(command, f"{option[1]} {QFileDialog.getSaveFileName(self, "ROM selection", "All Files(*);;Text Files(*.txt)", options = fdOptions)[0]}") if command else "")                
+                        if line_edit_of_rom:
+                            btn.clicked.connect(lambda _,: self.enable_run_button(command, f"{option[1]} {shlex.quote(QFileDialog.getOpenFileName(self, "File selection", '')[0])} {shlex.quote(line_edit_of_rom.text())}") if command else "")   
+                        else:
+                            btn.clicked.connect(lambda _,: self.enable_run_button(command, f"{option[1]} {shlex.quote(QFileDialog.getOpenFileName(self, "File selection", '')[0])}") if command else "")                
+                    elif type_of_param == Types.PATH_SET_LOCATION:
+                        btn = QPushButton("Save File As...")
+                        btn.setToolTip(option[4])
+                        fdOptions = QFileDialog.Option.ShowDirsOnly
+                        btn.clicked.connect(lambda _,: self.enable_run_button(command, f"{option[1]} {QFileDialog.getSaveFileName(self, "Location selection", "All Files(*)", options = fdOptions)[0]}") if command else "")                
                     elif type_of_param == Types.SPINNER:
                         btn = QSpinBox()
                         btn.setRange(0, 65535)
@@ -177,8 +207,30 @@ class SC64MainWidget(QWidget):
                         i += 1
                     self.parameters_of_options.append(btn)
         else: # If there are no parameters, run the command directly
-            self.run_command(command, option[1])
+            #self.run_command(command, option[1])
+            self.enable_run_button(command, option[1])
 
+
+
+    ### PROCESS PART ###
+
+    def enable_run_button(self, command):
+        self.run_command_button.setEnabled(True)
+        self.run_command_button.setStyleSheet("background-color: lightgreen;")
+        self.run_command_button.clicked.connect(lambda: self.run_command(command))
+
+    def enable_run_button(self, command, options=""):
+        self.run_command_button.setEnabled(True)
+        self.run_command_button.setStyleSheet("background-color: lightgreen;")
+        if self.run_command_button.receivers(self.run_command_button.clicked) > 0:
+            self.run_command_button.clicked.disconnect()
+        self.run_command_button.clicked.connect(lambda: self.run_command(command, options))
+
+    def disable_run_button(self):
+        self.run_command_button.setStyleSheet("")
+        self.run_command_button.setEnabled(False)
+        if self.run_command_button.receivers(self.run_command_button.clicked) > 0:
+            self.run_command_button.clicked.disconnect()
 
     def run_command(self):
         if not self.exe_path:
@@ -196,10 +248,17 @@ class SC64MainWidget(QWidget):
         if not self.exe_path:
             self.append_error("Please select sc64deployer.exe first.")
             return
+        ## Special case for no command
+        if command == '---' :
+                command = list(options.split())[0]
+                options = ""
+                if len(list(options.split())) >= 2 and list(options.split())[1] is not None:
+                    options = ' '.join(list(options.split())[1:])
         self.text_output.append(f"\n[CMD] {command} {options}")
         self.text_output.moveCursor(QTextCursor.MoveOperation.End)
         for btn in self.buttons:
             btn.setEnabled(False)
+
         self.start_worker(command, options=options)
 
     def start_worker(self, command, options = "", notify=True):
@@ -209,7 +268,8 @@ class SC64MainWidget(QWidget):
             self.worker_thread.quit()
             self.worker_thread.wait()
         # Set up new worker and thread
-        self.worker = ExecWorker(self.exe_path, command, list(options.split()) if options else [])
+        args = shlex.split(options) if options else []
+        self.worker = ExecWorker(self.exe_path, command, args)
         self.worker_thread = QThread()
         self.worker.moveToThread(self.worker_thread)
         self.worker.output.connect(self.append_output)
@@ -217,6 +277,15 @@ class SC64MainWidget(QWidget):
         self.worker.finished.connect(self.process_finished)
         self.worker_thread.started.connect(self.worker.run)
         self.worker_thread.start()
+
+
+    ### END PROCESS PART ###
+
+
+
+
+
+    ### CONSOLE OUTPUT PART ###
 
     def append_output(self, text):
         if self.notify:
@@ -231,6 +300,10 @@ class SC64MainWidget(QWidget):
     def append_error(self, text):
         self.text_output.append(f"{text.strip()}")
         self.text_output.moveCursor(QTextCursor.MoveOperation.End)
+
+    ### END CONSOLE OUTPUT PART ###
+
+
 
 
     def process_finished(self):
@@ -286,7 +359,9 @@ class SC64MainWindow():
         self.main_widget.check_set_exe()  # Check if exe is set
 
 
-    ### Settings management
+    ### SETTINGS PART ###
+
+
     def write_settings(self):
         settings = QSettings("SC64 deployer Gui", "Settings")
         settings.beginGroup("MainWindow")
@@ -302,9 +377,10 @@ class SC64MainWindow():
         settings.beginGroup("MainWindow")
         self.main_widget.exe_path = settings.value("exe_path", None)
         self.window.resize(settings.value("size", QSize(800, 600)))
-    ###########
+    
+    
+    ### END SETTINGS PART ###
         
-
     def run(self):
         self.window.show()
         self.app.exec()
